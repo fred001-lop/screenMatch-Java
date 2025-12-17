4f6a98ab30d3813323d80a0fd2e01de2c57b1d36
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-16 22:35:37 -0600

    Atribuyendo valores

--- src/Principal.java
+++ src/Principal.java
@@ -7,12 +7,18 @@ public class Principal {
         miPelicula.duracion = 120;
 
 
-        System.out.println(miPelicula.nombre);
-        System.out.println(miPelicula.fechaLanzamiento);
-        System.out.println(miPelicula.duracion);
+        System.out.println("Mi pelicula es: " + miPelicula.nombre);
+        System.out.println("La fecha de lanzamiento es: "+ miPelicula.fechaLanzamiento);
+        System.out.println("La duracion de la pelicula es: " +miPelicula.duracion);
 
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaLanzamiento = 1999;
+        otraPelicula.duracion = 60;
 
-
+        System.out.println("Mi pelicula es: " + otraPelicula.nombre);
+        System.out.println("La fecha de lanzamiento es: "+ otraPelicula.fechaLanzamiento);
+        System.out.println("La duracion de la pelicula es: " +otraPelicula.duracion);
 
     }
 }
