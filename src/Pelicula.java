7e724baa05fe96ac583a7b4068f5559eb5d8cad6
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-12-18 15:47:15 -0600

    Creacion de dos funciones una que suma y la otra calcula las evevaluaciones

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,11 +3,25 @@ public class Pelicula {
     int fechaLanzamiento;
     int duracion;
     boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalEvaluaciones;
 
     void muestraFichaTecnica(){
         System.out.println("Mi pelicula es: " + nombre);
         System.out.println("La fecha de lanzamiento es: "+ fechaLanzamiento);
         System.out.println("La duracion de la pelicula es: " +duracion);
+    }
+
+    void evalua(double nota){
+
+        sumaDeLasEvaluaciones += nota;
+        totalEvaluaciones++;
+
+
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalEvaluaciones;
     }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -7,13 +7,28 @@ public class Principal {
         miPelicula.duracion = 120;
 
         miPelicula.muestraFichaTecnica();
-        
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+
+
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
         otraPelicula.fechaLanzamiento = 1999;
         otraPelicula.duracion = 60;
 
-       otraPelicula.muestraFichaTecnica();
+        otraPelicula.evalua(5);
+        otraPelicula.evalua(10);
+        otraPelicula.evalua(10);
+        
+        System.out.println(otraPelicula.sumaDeLasEvaluaciones);
+        System.out.println(otraPelicula.totalEvaluaciones);
+        System.out.println(otraPelicula.calculaMedia());
+
+//       otraPelicula.muestraFichaTecnica();
 
     }
 }
